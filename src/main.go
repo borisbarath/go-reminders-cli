aeb07973db3d8a67b7cdba7368e1bc81131fa3ed
Boris Barath <boris.barath@gmail.com>
2021-10-11 21:55:22 +0100

    support for deleting done reminders and for listing reminders in a list

--- src/main.go
+++ src/main.go
@@ -13,6 +13,12 @@ var (
 	addText = add.Arg("text", "Text of the reminder to create").Required().String()
 
 	lists = kingpin.Command("lists", "Print out the available lists of reminders")
+
+	list     = kingpin.Command("list", "Print out the reminders in a list")
+	listName = list.Arg("list name", "Name of the list whose contents to show").Required().String()
+
+	purge     = kingpin.Command("purge", "Delete all completed reminders (from a specified list")
+	purgeList = purge.Arg("list", "The list whose reminders to clear, defaults to all").Default("all-lists").String()
 )
 
 func main() {
@@ -24,6 +30,10 @@ func main() {
 		remindersinterface.AddNewReminder(*addList, *addText)
 	case "lists":
 		remindersinterface.GetReminderListNames()
+	case "list":
+		remindersinterface.GetRemindersFromList(*listName)
+	case "purge":
+		remindersinterface.Purge(*purgeList)
 	default:
 		fmt.Println("Error: Please enter a command (add/lists)")
 	}
--- src/remindersinterface/remindersinterface.go
+++ src/remindersinterface/remindersinterface.go
@@ -2,23 +2,71 @@ package remindersinterface
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/andybrewer/mack"
 )
 
 func GetReminderListNames() {
 	listCommand := `
-	get name of lists`
+	get name of lists
+	`
 	listNames, _ := mack.Tell("Reminders", listCommand)
 	fmt.Println(listNames)
 }
 
+func GetRemindersFromList(listName string) {
+	cmdTemplate := `
+	set myList to list "%s"
+	tell myList
+		set names to name of reminders whose completed is false 
+		set listTexts to ""
+		set iterator to 0
+		repeat with reminderName in names 
+			set listTexts to listTexts & reminderName & "$next"
+			set iterator to iterator + 1
+		end repeat
+		listTexts
+	end tell
+	`
+	cmd := fmt.Sprintf(cmdTemplate, listName)
+	reminderNames, _ := mack.Tell("Reminders", cmd)
+	for index, reminderText := range strings.Split(reminderNames, "$next") {
+		if reminderText != "" {
+			fmt.Println(index, reminderText)
+		}
+	}
+}
+
+func Purge(listName string) {
+	cmd := ""
+	if listName == "all-lists" {
+		cmd = `
+		set myLists to name of every list
+		repeat with currList in myLists
+			tell list currList
+				delete (every reminder whose completed is true)
+			end tell
+		end repeat
+		`
+	} else {
+		cmdTemplate := `
+		set myList to list "%s"
+		tell myList
+			delete (every reminder whose completed is true)
+		end tell
+		`
+		cmd = fmt.Sprintf(cmdTemplate, listName)
+	}
+	mack.Tell("Reminders", cmd)
+}
+
 func AddNewReminder(listName string, reminderText string) {
-	cmd := `
+	cmdTemplate := `
 	set myList to list "%s" 
 	tell myList
 		make new reminder at end with properties {name:"%s"}
 	end tell`
-	addCommand := fmt.Sprintf(cmd, listName, reminderText)
-	mack.Tell("Reminders", addCommand)
+	cmd := fmt.Sprintf(cmdTemplate, listName, reminderText)
+	mack.Tell("Reminders", cmd)
 }
